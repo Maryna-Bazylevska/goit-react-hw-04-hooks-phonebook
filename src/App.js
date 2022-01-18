4e552b1bed6eb1bd355edd9eddc3ef989caa1fa5
Maryna-Bazylevska <83463971+Maryna-Bazylevska@users.noreply.github.com>
2022-01-18 16:38:22 +0100

    correction

--- src/App.js
+++ src/App.js
@@ -6,10 +6,19 @@ import ContatctList from "./Components/ContactList";
 import Filter from "./Components/Filter";
 import Notification from "./Components/Notification";
 export default function App() {
-  const [contacts, setContacts] = useState([]);
+  const [contacts, setContacts] = useState(() => {
+    return (
+      JSON.parse(localStorage.getItem("contacts")) ?? [
+        { id: "id-1", name: "Rosie Simpson", number: "459-12-56" },
+        { id: "id-2", name: "Hermione Kline", number: "443-89-12" },
+        { id: "id-3", name: "Eden Clements", number: "645-17-79" },
+        { id: "id-4", name: "Annie Copeland", number: "227-91-26" },
+      ]
+    );
+  });
   const [filter, setFilter] = useState("");
   useEffect(() => {
-    localStorage.setItem("contacts", JSON.stringify(contacts));
+    window.localStorage.setItem("contacts", JSON.stringify(contacts));
   }, [contacts]);
   const addContact = ({ name, number }) => {
     const contact = {
